6d726d738eb9f9c96b39813d648a330a485ba9c6
Tarun Dubey <tarun2019dubey@gmail.com>
2025-03-07 16:37:42 +0530

    Tarun add. by name

--- HelloWorldApp/src/app/app.component.ts
+++ HelloWorldApp/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FormsModule],
+
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -17,4 +20,6 @@ export class AppComponent {
   openBridgeLabz(): void {
     window.open("https://www.bridgelabz.com", "_blank");
   }
+
+  userName: string="";
 }
